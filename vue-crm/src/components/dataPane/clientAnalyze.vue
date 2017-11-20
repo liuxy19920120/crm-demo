c1bd3b650e8ddf67b94bbbac970f31f8938d1b81
liuxy19920120 <1594680127@qq.com>
2017-11-20 14:01:25 +0800

    可视化图表

--- vue-crm/src/components/dataPane/clientAnalyze.vue
+++ vue-crm/src/components/dataPane/clientAnalyze.vue
@@ -1,13 +1,132 @@
 <template>
-  <div>111</div>
+  <div class="echartPane" :style="{height:echartPaneHeight+'px'}">
+     <Card class="echartPaneItem">
+        <div class="paneContent" ref="customerClient">
+
+        </div>
+        <div class="threadWrap">查看详情</div>
+    </Card>
+     <Card class="echartPaneItem">
+        <div class="paneContent" ref="echartPaneItem">
+
+        </div>
+        <div class="threadWrap">查看详情</div>
+    </Card>
+  </div>
 </template>
 
 <script>
+import echarts from 'echarts'
 export default {
+    components:[
+        echarts
+    ],
+    mounted () {
+        // 基于准备好的dom，初始化echarts实例
+        var customerClient = echarts.init(this.$refs.customerClient)
+        // 绘制图表
+        customerClient.setOption( {
+        color: ['#3398DB'],
+        title: {
+            text: '员工客户情况统计',
+            left: 'left'
+        },
+        tooltip : {
+            trigger: 'axis',
+            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        legend:{
+            left: 'center',
+            top: '8%',  
+            data:[{name:'客户数',icon:'roundRect'},{name:'联系人数',icon:'roundRect'}]
+        },
+        grid: {
+            top:'20%',
+            left: '3%',
+            containLabel: true
+        },
+        xAxis : [
+            {
+                type : 'category',
+                data : ['客户数', '联系人数'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value'
+            }
+        ],
+        series : [
+            {
+                type: 'bar',
+                name:'线索名称',
+                data:[
+                    {value:5,name: '客户数'},
+                    {value:2, name: '联系人数'}
+                ],
+            }
+        ]
+        })
+    },
+    computed:{
+        layoutHeight:{ // 容器高度
+            get(){
+                return this.$store.state.layoutHeight
+            },
+            set(){
+
+            }
+        },
+        echartPaneHeight:{
+            get(){
+                return this.layoutHeight - 116
+            },
+            set(value){
+
+            }
+        }
+    }
 
 }
 </script>
 
 <style>
-
+.echartPane{
+    padding: 20px;
+    background: #eee;
+    display: flex;
+    justify-content:space-between;
+}
+.echartPaneItem{
+    width: 502px;
+    height: 328px;
+    background: #fff;
+    position: relative;
+}
+.paneContent{
+    width: 502px;
+    height: 328px;
+}
+.echartPaneItem:hover .threadWrap{
+    display: block;
+}
+.threadWrap{
+    display: none;
+    cursor: pointer;
+    width: 502px;
+    height: 328px;
+    position: absolute;
+    text-align: center;
+    line-height: 328px;
+    color:#fff;
+    font-weight: bold;
+    left: 0px;
+    top:0px;
+    background: rgba(88,94,107,.4)
+}
 </style>
--- vue-crm/src/components/dataPane/salesThreadAnalyze.vue
+++ vue-crm/src/components/dataPane/salesThreadAnalyze.vue
@@ -7,7 +7,7 @@
         <div class="threadWrap">查看详情</div>
     </Card>
      <Card class="echartPaneItem">
-        <div class="paneContent" ref="echartPaneItem">
+        <div class="paneContent" ref="threadStatus">
 
         </div>
         <div class="threadWrap">查看详情</div>
@@ -23,9 +23,9 @@ export default {
     ],
     mounted () {
         // 基于准备好的dom，初始化echarts实例
-        var myChart = echarts.init(this.$refs.threadChangeProcess)
+        var threadChangeProcessChart = echarts.init(this.$refs.threadChangeProcess)
         // 绘制图表
-        myChart.setOption( {
+        threadChangeProcessChart.setOption( {
             title: {
                 text: '线索转化过程分析',
                 left: 'left'
@@ -36,19 +36,21 @@ export default {
             legend: {
                 left: 'center',
                 top: '8%',               
-                data: ['线索数', '转客户的线索数','转联系人的线索数','转换的客户数','转换的联系人数']
+                data: [{name:'线索数',icon:'roundRect'}, {name:'转客户的线索数',icon:'roundRect'}, {name:'转联系人的线索数',icon:'roundRect'},'转换的客户数','转换的联系人数']
             },
             xAxis: {
+                type: 'category',
+                boundaryGap: false,
                 splitLine: {show: true},
                 data: ['电话', '广告', '会议', '客户介绍', '其他', '搜索引擎', '网站']
             },
             grid: {
                 left: '1%',
-                top: '20%',
+                top: '25%',
                 containLabel: true
             },
             yAxis: {
-                type: 'log',
+                type: 'value'
             },
             series: [
                 {
@@ -57,11 +59,63 @@ export default {
                     data: [1, 3, 9, 27, 81, 247, 741]
                 },
                 {
-                    name: '广告',
+                    name: '转客户的线索数',
                     type: 'line',
                     data: [1, 2, 4, 8, 16, 32, 64]
                 },
+                {
+                    name: '转联系人的线索数',
+                    type: 'line',
+                    data: [4, 6, 32, 80, 16, 32,54]
+                },
+                {
+                    name: '转换的客户数',
+                    type: 'line',
+                    data: [10, 20, 40, 8, 16, 23, 10]
+                },
+                {
+                    name: '转换的联系人数',
+                    type: 'line',
+                    data: [10, 20, 90, 12, 19, 12, 20]
+                }
             ]
+        })
+         // 基于准备好的dom，初始化echarts实例
+        var threadStatusChart = echarts.init(this.$refs.threadStatus)
+        // 绘制图表
+        threadStatusChart.setOption( {
+        title: {
+            text: '线索状态分布',
+            left: 'left'
+        },
+        tooltip : {
+            trigger: 'item',
+            formatter: "{a}<br>{b} : {c} ({d}%)"
+        },
+        series : [
+            {
+                type: 'pie',
+                radius : '65%',
+                center: ['50%', '50%'],
+                selectedMode: 'single',
+                name:'线索名称',
+                data:[
+                    {value:0,name: '已作废'},
+                    {value:0, name: '待处理'},
+                    {value:0, name: '跟进中'},
+                    {value:0, name: '未分配'},
+                    {value:0, name: '无效'},
+                    {value:0, name: '已转换'}
+                ],
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
         })
     },
     computed:{
